68a814ed929b21bb6365ed814651a1c21126b31c
lux-ed <lux-ed@users.noreply.github.com>
2018-11-30 10:53:07 +0100

    Added layers

--- map1.py
+++ map1.py
@@ -16,19 +16,22 @@ def color_producer(elevation):
         return 'red'
          
 map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Mapbox Bright")
-fg = folium.FeatureGroup(name="My map")
-#put the latitude and longitude into a list
+
+fgv = folium.FeatureGroup(name="Volcanoes") #if no FeatureGroup, it adds each volcano as a child.
+
 
 for lt, ln, el in zip(lat, lon, elev):
-    fg.add_child(folium.CircleMarker(location=[lt, ln], radius= 6, popup=str(el)+ " m",
+    fgv.add_child(folium.CircleMarker(location=[lt, ln], radius= 6, popup=str(el)+ " m",
     fill_color=color_producer(el), color='grey', fill_opacity=0.7))
 
-fg.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(), style_function=lambda x: {'fillColor':'yellow'}))
+fgp = folium.FeatureGroup(name="Population")  
+
+fgp.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(),
+style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000
+else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
 
 
-# for coordinates in [[38.2, -99.1], [39.2, -97.1]]:   #iterating through locations.
-#     fg.add_child(folium.Marker(location=coordinates, popup="Hi I am a marker", icon=folium.Icon(color='green')))
-
-map.add_child(fg)   #its good to keep the code organize, 111
-
+map.add_child(fgv)
+map.add_child(fgp)   
+map.add_child(folium.LayerControl())
 map.save("Map1.html")
